058ba3accf95a1d2bd5f14e983df9ff18e94353f
azazel <384735481@qq.com>
2025-11-25 17:02:37 +0800

    Add some new things & Fix bundle

--- src/aztest/content/TestBlocks.java
+++ src/aztest/content/TestBlocks.java
@@ -12,13 +12,14 @@ import mindustry.world.blocks.production.GenericCrafter;
 public class TestBlocks {
     public static Block
             //铁矿产线
-            furnace, processingFactory,
+            furnace, processingFactory,gearAssemblyMachine,
             //矿石
             ironOre,
             //炮台
             fireBull;
 
     public static void load() {
+        //熔炉
         furnace = new GenericCrafter("furnace"){{
             requirements(Category.crafting, ItemStack.with(Items.copper,100,Items.lead,80, TestItems.iron,30));//建造所需100个铜 80个铅 30个铁矿
 
@@ -33,6 +34,7 @@ public class TestBlocks {
             craftTime = 90f;
         }};
 
+        //制钉厂
         processingFactory = new GenericCrafter("processingFactory"){{
             requirements(Category.crafting, ItemStack.with(Items.copper,100,Items.lead,80, TestItems.ironPlate,30));//建造所需100个铜 80个铅 30个铁矿
 
@@ -47,12 +49,27 @@ public class TestBlocks {
             craftTime = 110f;
         }};
 
+        //齿轮组装机
+        gearAssemblyMachine = new GenericCrafter("gearAssemblyMachine"){{
+           requirements(Category.crafting, ItemStack.with(Items.copper,100,Items.lead,80, TestItems.iron,30));
+           alwaysUnlocked = true;
+           craftEffect = Fx.pulverizeMedium;
+           outputItem = new ItemStack(TestItems.gear,1);
+           size = 2;
+           hasItems = true;
+           craftTime = 110f;
+           consumePower(0.5f);
+           consumeItems(ItemStack.with(TestItems.ironPlate,4,TestItems.nails,4));
+        }};
+
+        //铁矿石
         ironOre = new OreBlock(TestItems.iron){{
             oreDefault = true;
             oreThreshold = 0.81f;
             oreScale = 23.47619f;
         }};
 
+        //钉子炮塔
         fireBull = new ItemTurret("fire"){{
             alwaysUnlocked = true;
             ammo();
--- src/aztest/content/TestItems.java
+++ src/aztest/content/TestItems.java
@@ -8,23 +8,48 @@ public class TestItems {
     ///通过bundles/bundle_zh_CN.properties进行多语言化
     public static Item
             //铁的产线
-            iron,ironPlate,nails;
+            iron,ironPlate,nails,gear,
+            //钢铁工艺
+            steel,steelPlate
+            ;
 
     public static void load(){
+        //铁锭
         iron = new Item("iron", Color.lightGray){{
             hardness = 1;
             cost = 0.5f;
             alwaysUnlocked = true;
         }};
 
+        //铁板
         ironPlate = new Item("iron_plate",Color.gray){{
             cost = 0.6f;
             hardness = 1;
             alwaysUnlocked = false;
         }};
 
+        //钉子
         nails = new Item("nails",Color.darkGray){{
             cost = 0.5f;
+            hardness = 1;
+            alwaysUnlocked = false;
+        }};
+
+        //齿轮
+        gear = new Item("gear",Color.gray){{
+           cost = 0.5f;
+           hardness = 1;
+           alwaysUnlocked = false;
+        }};
+
+        steel = new Item("steel",Color.gray){{
+            cost = 0.6f;
+            hardness = 1;
+            alwaysUnlocked = false;
+        }};
+
+        steelPlate = new Item("steel_plate",Color.gray){{
+            cost = 0.7f;
             hardness = 1;
             alwaysUnlocked = false;
         }};
